941bea68731c0fd5306a6f277159e0e92c1b8aea
Nikita Tumashevich <ntumasevic@gmail.com>
2022-06-17 14:11:51 +0300

    .

--- Assets/Script/tower/TowerRemoval.cs
+++ Assets/Script/tower/TowerRemoval.cs
@@ -5,9 +5,18 @@ using UnityEngine;
 public class TowerRemoval : MonoBehaviour
 {
     public PlaceTower Object;
+    private GameManagerBehavior gameManagere;  // to access the GameManager Behavior component of the scene's GameManager object
 
     void OnMouseUp()
     {
-        Object.SetTower(null);
+        Destroy(Object.GetTower());
+        gameManagere.Gold += Object.GetTower().GetComponent<TowerData>().CurrentLevel.refundAmount;
+        Object.transform.Find("tower work").gameObject.SetActive(false);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameManagere = GameObject.Find("GameManager").GetComponent<GameManagerBehavior>();
     }
 }
--- Assets/script/tower/TowerData.cs
+++ Assets/script/tower/TowerData.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class TowerLevel
 {
     public int cost;
+    public int refundAmount;
     public GameObject visualization;
 }
 
